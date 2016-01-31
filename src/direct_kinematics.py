6ff56773f2e53cb229daee7f9c844dc5268ce5a8
Rack07 <rack.kendorim@gmail.com>
2016-01-31 20:34:55 +0100

    Réorganisation légère

--- src/direct_kinematics.py
+++ src/direct_kinematics.py
@@ -4,8 +4,8 @@ L1 = 51.0
 L2 = 63.7
 L3 = 93.0
 
-Alpha = math.radians(20.69)
-Beta = math.radians(5.06)
+Alpha = math.radians(20.69)	#Conversion des degrés en radian
+Beta = math.radians(5.06)	#Conversion des degrés en radian
 
 def radian_validation (radian):
 	return (radian <= 2 * math.pi and radian >= -2 * math.pi)
@@ -13,35 +13,32 @@ def radian_validation (radian):
 def leg_dk(theta1, theta2, theta3, l1=L1, l2=L2, l3=L3, alpha = Alpha, beta = Beta):
 	x = y = z = "error"
 	if radian_validation(theta1) and radian_validation(theta2) and radian_validation(theta3) and radian_validation(alpha) and radian_validation(beta):
-		c_2 = math.cos(theta2)
-		d_12 = L2 * c_2;
-		
-		c_2_3 = math.cos(theta2 + theta3)
-		d_23 = L3 * c_2_3
-		
-		planContribution = (L1 + (L2 * c_2) + (L3 * c_2_3)) 
-		
+		#Calcul des différents cosinus et sinus
 		c_1 = math.cos(theta1)
+		c_2 = math.cos(theta2)
+		c_2_3 = math.cos(theta2 + theta3)
 		c_a = math.cos(alpha)
-		s_a = math.sin(alpha)
 		c_b = math.cos(beta)
-		s_b = math.sin(beta)
-		delta_x = (-1 * (L2 * (1 - c_a) * c_1) - (L3 * (1 - s_b) * c_1))
-		x = (planContribution * c_1) + delta_x
-		
 		s_1 = math.sin(theta1)
-		delta_y = (-1 * (L2 * (1 - c_a) * s_1) - (L3 * (1 - s_b) * s_1))
-		y = (planContribution * s_1) + delta_y
-		
 		s_2 = math.sin(theta2)
 		s_2_3 = math.sin(theta2 + theta3)
-		delta_z = -1 * (L2 * s_a) - (L3 * c_b)
-		z = (L2 * s_2) + (L3 * s_2_3) + delta_z
+		s_a = math.sin(alpha)
+		s_b = math.sin(beta)
+		#Calcul des projections
+		projection_delta = (l2 * (1 - c_a)) + (l3 * (1 - s_b)) 
+		projection = l1 + (l2 * c_2) + (l3 * c_2_3) 
+		delta_x = c_1*projection_delta
+		delta_y = s_1*projection_delta
+		delta_z = (l2 * s_a) + (l3 * c_b)
+		#Calcul de la position
+		x = (projection * c_1) - delta_x
+		y = (projection * s_1) - delta_y
+		z = (l2 * s_2) + (l3 * s_2_3) - delta_z
 	
 	print ("["+ str(x) +", " + str(y) + ", " + str(z) +"]")
 
 
-leg_dk(math.radians(0), math.radians(0), math.radians(0))
-leg_dk(math.radians(90), math.radians(0), math.radians(0))
-leg_dk(math.radians(180), math.radians(-30.501), math.radians(-67.819))
-leg_dk(math.radians(0), math.radians(-30.645), math.radians(38.501))
+leg_dk(theta1=math.radians(0), theta2=math.radians(0), theta3=math.radians(0))
+leg_dk(theta1=math.radians(90), theta2=math.radians(0), theta3=math.radians(0))
+leg_dk(theta1=math.radians(180), theta2=math.radians(-30.501), theta3=math.radians(-67.819))
+leg_dk(theta1=math.radians(0), theta2=math.radians(-30.645), theta3=math.radians(38.501))
