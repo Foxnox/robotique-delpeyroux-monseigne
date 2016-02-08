5dfd934c94cbf6fab5da494cf1f1a7c44bc7696a
Thibaut Monseigne <tmonseigne@emi.u-bordeaux1.fr>
2016-02-08 13:33:35 +0100

    Fix DK et ajouts com. IK

--- src/inverse_kinematics.py
+++ src/inverse_kinematics.py
@@ -9,7 +9,7 @@ L3 = 93.0
 Alpha = radians(20.69)	#Get the radian angle for alpha
 Beta = radians(5.06)	#Get the radian angle for beta
 
-# return opppisite angle of the side a of the tringle defined by a, b and c 
+# return opppisite angle of the side a of the triangle defined by a, b and c 
 def al_kashi (a, b , c):
 	return acos((b*b + c*c - a*a)/(2 * b * c))
 	
@@ -17,34 +17,37 @@ def al_kashi (a, b , c):
 def leg_ik(x, y, z, l1=L1, l2=L2, l3=L3, alpha = Alpha, beta = Beta):
 	l_max = l1+l2+l3
 	l_proj = sqrt(x*x + y*y)
-	if ((x*x + y*y + z*z) <= l_max*l_max):
-		d13 = l_proj - l1
-		d = sqrt(z*z + d13*d13)
-	
-		if (d13 == 0):
-			a = 0
+	if ((x*x + y*y + z*z) <= l_max*l_max):	#On limite à une sphère de rayon longueur max 
+		d13 = l_proj - l1					#Distance entre P1 et le projeté de P3
+		d = sqrt(z*z + d13*d13)				#Distance entre P1 et P3
+		
+		#Calcul de a l'angle P3proj-P1-P3
+		if (d13 == 0):						#Si la jambe est tendu vers le bas ou le haut
+			a = 0			
 		else : 
 			a = atan2(z, d13)
 		
-		if (d==0):
-			b = 0
+		#Calcul de b l'angle P2-P1-P3
+		if (d==0):							#Cas mécaniquement impossible de toute façon (car L3!=L2)
+			b = 0							
 		else:
 			b = al_kashi(l3, l2, d)
 		
-		if (x == 0):
-			theta1 = pi/2
-		elif (x < 0 and y == 0) :
-			theta1=pi
+		if (x == 0):						#On evite les méchantes divisions par 0 
+			theta1 = pi/2					#90°
+		elif (x < 0 and y == 0) :			#On est sur l'autre cadran 
+			theta1=pi						#180°
 		else:
-			theta1 = atan2(y, x)
+			theta1 = atan2(y, x)			#==atan(y/x)
 			
-		theta2 = - (b + a + alpha)
-		theta3 = - (pi - al_kashi(d, l3, l2) - ((pi/2) - alpha - beta))
+		theta2 = - (b + a + alpha)			#alpha est la compensation mécanique
+		theta3 = - (pi - al_kashi(d, l3, l2) - ((pi/2) - alpha - beta))	#180 - angle P1-P2-P3 - Compensation mecanique
 			
 		angle = Vertex(degrees(theta1), degrees(theta2), degrees(theta3))
 		print("Voici les angles en degrees : " + str(angle))
 	else:
-		print("C'est pas une super pate extensible")
+		print("Ce n'est pas une super pate extensible")
+	#Pour le moment on ne gère qu'une longueur max il faudra prendre en compte la conjugaison des 3 angles en fonctions des L1, L2, L3 pour précisé les lieux impossibles
 
 	
 leg_ik(118.79, 0.0, -115.14)
