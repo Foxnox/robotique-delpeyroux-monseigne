8e35e6b3edf0ec262583fde021fa324178c0998e
Arnaud Delpeyroux <adelpeyr@emi.u-bordeaux1.fr>
2016-03-30 17:38:39 +0200

    control by mouse

--- src/megabot.py
+++ src/megabot.py
@@ -7,6 +7,9 @@ import math
 from contextlib import closing
 import pypot.robot
 
+import pygame
+from pygame.locals import *
+
 center_pos = [100, 0, -100]
 
 def get_position (angles):
@@ -117,6 +120,34 @@ def make_circle(megabit):
 	delta=50
 	
 
+pygame.init()
+H_Fenetre = 600
+L_Fenetre = 600
+H_Reticule = 50
+L_Reticule = 50
+
+#Ouverture de la fenetre Pygame
+fenetre = pygame.display.set_mode((L_Fenetre, H_Fenetre))
+
+#Chargement et collage du fond
+fond = pygame.image.load("../Ressources/Grille.jpg").convert()
+fond = pygame.transform.scale(fond, (L_Fenetre, H_Fenetre))
+fenetre.blit(fond, (0,0))
+
+
+#Chargement et collage du personnage
+Reticule = pygame.image.load("../Ressources/Reticule.png").convert_alpha()
+Reticule = pygame.transform.scale(Reticule, (L_Reticule, H_Reticule))
+Reticulte_x = L_Fenetre/2-L_Reticule/2
+Reticulte_y = H_Fenetre/2-H_Reticule/2
+fenetre.blit(Reticule, (Reticulte_x, Reticulte_y))
+
+#Rafraichissement de l'ecran
+pygame.display.flip()
+pygame.key.set_repeat(400, 30)
+#BOUCLE INFINIE
+
+
 if __name__ == '__main__':
 	#On va maintenant utiliser un fichier de configuration
 	#Il nous permettra de limiter nos demandes aux angles valides
@@ -134,20 +165,33 @@ if __name__ == '__main__':
 		robot = corrected_robot([leg1, leg2, leg3, leg4, leg5, leg6])
 
 		robot.goto([0, 0])
-		
-		time.sleep(1)
-		
-		i = 0
-		f = 100
-		while True : 
-			bla = (1.0*i/f) * (2 * math.pi * 50) * 1.0/f 
-			x = math.cos(bla)*50
-			y = math.sin(bla)*50
+	
 			
-			robot.goto([x, y])
-			i += 1
-			
-			time.sleep(1.0/f)
+		continuer = 1
+		gaz=False
+		while continuer:
+			for event in pygame.event.get():	#Attente des evenements
+				if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+					continuer = 0
+				if event.type == MOUSEMOTION: #Si mouvement de souris
+					#On change les coordonnees du Reticule
+					Reticulte_x = event.pos[0]-L_Reticule/2
+					Reticulte_y = event.pos[1]-H_Reticule/2
+				if event.type == MOUSEBUTTONDOWN or (event.type == KEYDOWN and event.key == K_SPACE) :
+						gaz = not(gaz)
+						if gaz == 1 :
+							Reticule = pygame.image.load("../Ressources/Red_Reticule.png").convert_alpha()
+							Reticule = pygame.transform.scale(Reticule, (L_Reticule, H_Reticule))				
+						else:
+							Reticule = pygame.image.load("../Ressources/Reticule.png").convert_alpha()
+							Reticule = pygame.transform.scale(Reticule, (L_Reticule, H_Reticule))	
+								
+			#Re-collage
+			fenetre.blit(fond, (0,0))	
+			fenetre.blit(Reticule, (Reticulte_x, Reticulte_y))
+			robot.goto([(Reticulte_x - L_Fenetre/2)/6, (Reticulte_y - H_Fenetre/2)/6])
+			#Rafraichissement
+			pygame.display.flip()
 		
 
 
