a53f76e9934f3fc3b3f0fae35054c3b1cfa901ba
Arnaud Delpeyroux <arnaud.delpeyroux@live.com>
2016-04-05 20:21:51 +0200

    joy géré

--- src/gestion_evenement.py
+++ src/gestion_evenement.py
@@ -4,6 +4,7 @@ import numpy
 import inverse_kinematics
 import direct_kinematics
 import math
+import sys, os
 
 import pygame
 from pygame.locals import *
@@ -33,18 +34,42 @@ fenetre.blit(Reticule, (Reticulte_x, Reticulte_y))
 #Rafraichissement de l'ecran
 pygame.display.flip()
 pygame.key.set_repeat(400, 30)
+
+sys.stdout = os.devnull
+sys.stderr = os.devnull
+
+pygame.joystick.init()
 #BOUCLE INFINIE
 continuer = 1
 gaz=False
+
+
+is_mouse = True
+is_joystick = False
+joy_x_axis = 0
+joy_y_axis = 0
+
 while continuer:
+	try:
+		is_joystick = True
+		is_mouse = False
+		joystick = pygame.joystick.Joystick(0)
+		joystick.init()
+	
+		joy_x_axis = joystick.get_axis(0)
+		joy_y_axis = joystick.get_axis(1)
+	except:
+		is_joystick = False
+		is_mouse = True
+
 	for event in pygame.event.get():	#Attente des evenements
 		if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
 			continuer = 0
-		if event.type == MOUSEMOTION: #Si mouvement de souris
+		if event.type == MOUSEMOTION and is_mouse: #Si mouvement de souris
 			#On change les coordonnees du Reticule
 			Reticulte_x = event.pos[0]-L_Reticule/2
 			Reticulte_y = event.pos[1]-H_Reticule/2
-		if event.type == MOUSEBUTTONDOWN or (event.type == KEYDOWN and event.key == K_SPACE) :
+		if (event.type == MOUSEBUTTONDOWN or (event.type == KEYDOWN and event.key == K_SPACE)) and is_mouse :
 				gaz = not(gaz)
 				if gaz == 1 :
 					Reticule = pygame.image.load("../Ressources/Red_Reticule.png").convert_alpha()
@@ -52,7 +77,10 @@ while continuer:
 				else:
 					Reticule = pygame.image.load("../Ressources/Reticule.png").convert_alpha()
 					Reticule = pygame.transform.scale(Reticule, (L_Reticule, H_Reticule))	
-						
+	
+	if is_joystick :
+		Reticulte_x = 300 + (300 * joy_x_axis) -L_Reticule/2		
+		Reticulte_y = 300 + (300 * joy_y_axis) -H_Reticule/2		
 	#Re-collage
 	fenetre.blit(fond, (0,0))	
 	fenetre.blit(Reticule, (Reticulte_x, Reticulte_y))
